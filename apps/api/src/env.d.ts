a0f7c4d60fedea9b68960736846d3fc7bb3b3edd
Elias <e.wambugu192@gmail.com>
2022-10-22 17:00:14 -0400

    added cookie domain env

--- apps/api/src/env.d.ts
+++ apps/api/src/env.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     PORT: string;
     SESSION_SECRET: string;
+    COOKIE_DOMAIN: string;
   }
 }
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -56,6 +56,7 @@ const main = async () => {
       resave: false,
       saveUninitialized: false,
       cookie: {
+        domain: process.env.COOKIE_DOMAIN,
         secure: false,
         // secure: process.env.NODE_ENV === 'production' ? true : false, // if true: only transmit cookie over https, in prod, always activate this
         httpOnly: true, // if true: prevents client side JS from reading the cookie
