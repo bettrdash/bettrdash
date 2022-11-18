f954e49bbf5d0ba4fdb7c56232f1c9bf8922b5d0
Elias <e.wambugu192@gmail.com>
2022-11-18 08:51:13 -0800

    updated monitor table to be responsive on mobile

--- apps/website/src/Pages/Monitor.tsx
+++ apps/website/src/Pages/Monitor.tsx
@@ -11,6 +11,7 @@ import {
   Text,
   Flex,
   useColorModeValue,
+  Divider,
 } from "@chakra-ui/react";
 import { useQuery } from "react-query";
 import { ScaleLoader } from "react-spinners";
@@ -23,7 +24,7 @@ type ProjectProp = {
   live_url: string;
 };
 const Monitor = () => {
-  const color = useColorModeValue('#F2F2F2', '#171923')
+  const color = useColorModeValue("#F2F2F2", "#171923");
 
   const { data: projectMonitorData, status: projectMonitorStatus } = useQuery(
     "projectMonitor",
@@ -45,9 +46,45 @@ const Monitor = () => {
   const projects = projectMonitorData.projects;
   return (
     <>
-      <Heading textAlign={{base: 'center', md: 'start'}}>Monitor</Heading>
-      <Center h="100%">
-        <TableContainer
+      <Heading textAlign={{ base: "center", md: "start" }}>Monitor</Heading>
+      <Flex
+        h="100%"
+        overflowY="auto"
+        boxShadow={"lg"}
+        w="100%"
+        rounded={10}
+        mt={10}
+        flexDir="column"
+        p={4}
+      >
+        <Flex  w='100%' justify={'space-between'}>
+          <Heading fontSize={14}>URL</Heading>
+          <Heading fontSize={14}>Status</Heading>
+        </Flex>
+        {projects.map((project: ProjectProp, index: number) => (
+          <>
+            <Flex mt={5} justify="space-between">
+              <Text noOfLines={1} w={{base: '80%', md: '100%'}}>{project.live_url}</Text>
+              <Flex
+                ml={2}
+                rounded={5}
+                p={1}
+                fontSize={12}
+                color={color}
+                bg={project.status === "UP" ? "green.400" : "red.400"}
+              >
+                {project.status}
+                {project.status === "pending" ? (
+                  <Flex ml={2} alignSelf={"center"}>
+                    <ScaleLoader color={color} width={2} height={10} />
+                  </Flex>
+                ) : null}
+              </Flex>
+            </Flex>
+            <Divider mt={5} />
+          </>
+        ))}
+        {/* <TableContainer
           h="100%"
           overflowY="auto"
           boxShadow={"lg"}
@@ -90,8 +127,8 @@ const Monitor = () => {
               ))}
             </Tbody>
           </Table>
-        </TableContainer>
-      </Center>
+        </TableContainer> */}
+      </Flex>
     </>
   );
 };
