eda3715f7571e45c15d2cc5fc8b6d93b849a1143
Elias <e.wambugu192@gmail.com>
2023-01-05 16:05:15 -0500

    made analytic card text white

--- apps/website/src/Components/AnalyticCard.tsx
+++ apps/website/src/Components/AnalyticCard.tsx
@@ -14,6 +14,7 @@ const AnalyticCard = ({ title, value }: Props) => {
         w={{ base: "100%", md: 450 }}
         h={110}
         bgGradient={"linear(to-r, red.400,pink.400)"}
+        color='white'
       >
         <Heading fontSize={50}>{value}</Heading>
         <Text fontWeight={"600"}>{title}</Text>
